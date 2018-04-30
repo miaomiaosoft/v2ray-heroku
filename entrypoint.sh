82bf3e1a661fb57a525889eff6a20ef5314bd4d4
onplus <31188782+onplus@users.noreply.github.com>
2018-04-30 22:40:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 cd /v2raybin
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip v2ray.zip 
-cd /v2raybin/v2ray-v$VER-linux-64
-chmod +x v2ray
-chmod +x v2ctl
 
 echo -e -n "$CONFIG_JSON1" > config.json
 echo -e -n "$PORT" >> config.json
