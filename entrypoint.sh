2962069ef02ecd8fe079edea6add40ec4e264318
onplus <31188782+onplus@users.noreply.github.com>
2018-04-30 20:32:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ echo -e -n "$PORT" >> /etc/v2ray/config.json
 echo -e -n "$CONFIG_JSON2" >> /etc/v2ray/config.json
 echo -e -n "$UUID" >> /etc/v2ray/config.json
 echo -e -n "$CONFIG_JSON3" >> /etc/v2ray/config.json
-
+echo "0.0.0.0 $PORT 0.0.0.0 $PORT" > /etc/rinetd-bbr.conf
+ 
 rinetd -f -c /etc/rinetd-bbr.conf raw eth0
 v2ray -config=/etc/v2ray/config.json
